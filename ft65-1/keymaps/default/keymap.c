81e70fcce104556e7f43b643fecf8ebdff954c0b
Yago Santos <yagossc@gmail.com>
2020-10-12 17:23:21 -0300

    Swap caps and Fn layer key

--- ft65-1/keymaps/default/keymap.c
+++ ft65-1/keymaps/default/keymap.c
@@ -35,12 +35,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSLASH, KC_INSERT,
                      KC_Y, KC_J, KC_K, KC_L, KC_SCOLON, KC_GRAVE, KC_ENTER, TG(_MV),
                      KC_H, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT, KC_PGUP, KC_PSCREEN,
-                     KC_N, KC_SPACE, KC_RALT, KC_SLASH, KC_HOME, KC_PGDOWN, KC_END,
+                     KC_N, KC_SPACE, KC_RALT, KC_RGUI, KC_HOME, KC_PGDOWN, KC_END,
                      KC_QUOTE, KC_1, KC_2, KC_3, KC_4, KC_5,
                      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_6,
-                     KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_T,
+                     MO(_FN), KC_A, KC_S, KC_D, KC_F, KC_T,
                      KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_G,
-                     MO(_FN), KC_LCTRL, KC_LGUI, KC_LALT, KC_LCTRL, KC_B
+                     KC_CAPS, KC_LCTRL, KC_LGUI, KC_LALT, KC_LCTRL, KC_B
                      ),
     [_MV] = LAYOUT(
                      KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_NO, KC_NO, KC_NO,
